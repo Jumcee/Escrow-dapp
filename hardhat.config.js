eb34bef4d1926140bf3c9723a13ca90abc43088c
Jumcee <futhmah456@gmail.com>
2023-10-16 21:45:35 +0100

    escrow hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
 require('@nomicfoundation/hardhat-toolbox');
+require('dotenv').config();
 
 module.exports = {
   solidity: "0.8.17",
   paths: {
     artifacts: "./app/src/artifacts",
+  },
+  networks: {
+    sepolia: {
+      url: `${process.env.MY_ALCHEMY_RPC_ENDPOINT}`,
+      chainId: 11155111,
+      accounts: ["process.env.MY_PRIVATE_KEY"],
+    }
   }
 };
+
